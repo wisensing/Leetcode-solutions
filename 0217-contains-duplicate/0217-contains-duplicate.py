22626b8b3231adbf5dc0dbaff7fa45b101a4a900
Ruiqi <60522006+wisensing@users.noreply.github.com>
2023-11-06 23:42:13 +0100

    Time: 424 ms (67.74%), Space: 29 MB (59.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # def containsDuplicate(self, nums):
+    #     num_set = set()
+    #     for i in nums:
+    #         if i in num_set:
+    #             return True
+    #         else:
+    #             num_set.add(i)
+    #     return False
+    def containsDuplicate(self, nums):
+        num_set = set(nums)
+        if len(num_set) == len(nums):
+            return False
+        return True
+       
